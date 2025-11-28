fa06a954075a5103901c4d76ed4bbc93049721e3
humble dieu <dieuhieu10h@gmail.com>
2025-11-28 09:19:00 +0700

    feat(mailer): fixed logic verify email

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,11 @@
-import { BadRequestException, HttpException, Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NotFoundException,
+  UnprocessableEntityException,
+} from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
 import { I18nService } from 'nestjs-i18n';
@@ -17,12 +24,15 @@ export class AuthService {
     private jwtService: JwtService,
     private i18nService: I18nService<I18nTranslations>,
     private configService: ConfigService<AllConfigType>,
-    private readonly mailService: MailService
-  ) { }
+    private readonly mailService: MailService,
+  ) {}
 
   async validateUser(email: string, pass: string): Promise<any> {
     const user = await this.usersService.findByEmail(email);
-    const isValid = this.usersService.isValidPassword(pass, user?.password || '')
+    const isValid = this.usersService.isValidPassword(
+      pass,
+      user?.password || '',
+    );
     if (isValid) return user;
     return null;
   }
@@ -33,118 +43,176 @@ export class AuthService {
       name: signupDto.name,
       password: signupDto.password,
       dayOfBirth: signupDto.dayOfBirth,
-      gender: signupDto.gender
-    })
+      gender: signupDto.gender,
+    });
   }
 
   async logout(user: User) {
-    return await this.usersService.removeRefreshToken(user.id)
+    return await this.usersService.removeRefreshToken(user.id);
   }
 
   async login(user: User, response: Response) {
-    const { id, name, email, role, gender, dayOfBirth, avatar, publicId } = user
+    const { id, name, email, role, gender, dayOfBirth, avatar, publicId } =
+      user;
     const payload = {
       sub: 'token login',
       iss: 'server',
-      id, name, email, role
-    }
+      id,
+      name,
+      email,
+      role,
+    };
 
-    const refreshToken = this.createRefreshToken(payload)
+    const refreshToken = this.createRefreshToken(payload);
     response.cookie('refresh_token', refreshToken, {
       httpOnly: true,
-      maxAge: 2592000 * 1000
-    })
+      maxAge: 2592000 * 1000,
+    });
 
-    await this.usersService.updateUserToken(user, refreshToken)
+    await this.usersService.updateUserToken(user, refreshToken);
 
     return {
       access_token: this.jwtService.sign(payload, {
-        secret: this.configService.get('jwt.jwt_access_secret', { infer: true }),
-        expiresIn: this.configService.get('jwt.jwt_access_expiration_minutes', { infer: true })
+        secret: this.configService.get('jwt.jwt_access_secret', {
+          infer: true,
+        }),
+        expiresIn: this.configService.get('jwt.jwt_access_expiration_minutes', {
+          infer: true,
+        }),
       }),
       user: {
-        id, name, email, role, gender, dayOfBirth, avatar, publicId
-      }
-    }
+        id,
+        name,
+        email,
+        role,
+        gender,
+        dayOfBirth,
+        avatar,
+        publicId,
+      },
+    };
   }
 
   createRefreshToken = (payload: any) => {
     const refresh_token = this.jwtService.sign(payload, {
       secret: this.configService.get('jwt.jwt_refresh_secret', { infer: true }),
-      expiresIn: this.configService.get('jwt.jwt_refresh_expiration_days', { infer: true })
-    })
+      expiresIn: this.configService.get('jwt.jwt_refresh_expiration_days', {
+        infer: true,
+      }),
+    });
     return refresh_token;
-  }
+  };
 
   async processNewToken(refreshToken: string, response: Response) {
     try {
-      const user = await this.usersService.findUserByToken(refreshToken)
+      const user = await this.usersService.findUserByToken(refreshToken);
 
       if (user) {
-        const { id, name, email, role, gender, dayOfBirth } = user
+        const { id, name, email, role, gender, dayOfBirth } = user;
         const payload = {
           sub: 'token login',
           iss: 'server',
-          id, name, email, role,
-        }
+          id,
+          name,
+          email,
+          role,
+        };
 
         //save refresh token database
-        const refresh_token = this.createRefreshToken(payload)
-        this.usersService.updateUserToken(user, refresh_token)
-        response.clearCookie('refresh_token')
+        const refresh_token = this.createRefreshToken(payload);
+        this.usersService.updateUserToken(user, refresh_token);
+        response.clearCookie('refresh_token');
         response.cookie('refresh_token', refresh_token, {
           httpOnly: true,
-          maxAge: 2592000 * 1000
+          maxAge: 2592000 * 1000,
         });
 
         return {
           access_token: this.jwtService.sign(payload, {
-            secret: this.configService.get('jwt.jwt_access_secret', { infer: true }),
-            expiresIn: this.configService.get('jwt.jwt_access_expiration_minutes', { infer: true })
+            secret: this.configService.get('jwt.jwt_access_secret', {
+              infer: true,
+            }),
+            expiresIn: this.configService.get(
+              'jwt.jwt_access_expiration_minutes',
+              { infer: true },
+            ),
           }),
           user: {
-            id, name, email, role, gender, dayOfBirth
-          }
-        }
+            id,
+            name,
+            email,
+            role,
+            gender,
+            dayOfBirth,
+          },
+        };
       } else {
-        throw new NotFoundException(this.i18nService.t('common.NOT_FOUND', {
-          args: {
-            entity: "user"
-          }
-        }))
+        throw new NotFoundException(
+          this.i18nService.t('common.NOT_FOUND', {
+            args: {
+              entity: 'user',
+            },
+          }),
+        );
       }
     } catch (error) {
-      throw new BadRequestException(error.message)
+      throw new BadRequestException(error.message);
     }
   }
 
   async sendVerifyEmail(user: User) {
-    const token = this.jwtService.sign({
-      id: user.id,
-      name: user.name,
-      email: user.email
-    },
+    const token = this.jwtService.sign(
       {
-        secret: this.configService.get('jwt.jwt_confirm_email_secret', { infer: true }),
-        expiresIn: this.configService.get('jwt.jwt_verify_email_expiration_minutes', { infer: true })
-      }
-    )
+        id: user.id,
+        name: user.name,
+        email: user.email,
+      },
+      {
+        secret: this.configService.get('jwt.jwt_confirm_email_secret', {
+          infer: true,
+        }),
+        expiresIn: this.configService.get(
+          'jwt.jwt_verify_email_expiration_minutes',
+          { infer: true },
+        ),
+      },
+    );
     return this.mailService.verifyEmail({
       to: user.email,
-      data: { token }
-    })
+      data: { token },
+    });
   }
 
   async verifyEmail(token: string) {
     try {
       const isValidToken = this.jwtService.verify(token, {
-        secret: this.configService.get('jwt.jwt_confirm_email_secret', { infer: true })
-      })
+        secret: this.configService.get('jwt.jwt_confirm_email_secret', {
+          infer: true,
+        }),
+      });
+      const userData = this.jwtService.decode(token);
+
+      const { id, email } = userData;
+
+      const user = await this.usersService.findByEmail(email);
+
+      if (!user) {
+        throw new UnprocessableEntityException({
+          status: HttpStatus.UNPROCESSABLE_ENTITY,
+          errors: {
+            user: 'userNotFound',
+          },
+        });
+      }
+      // Change status of isEmailVerified
+      user.isEmailVerified = true;
+      const updatedUser = await this.usersService.updateUser(id, user);
 
       if (!isValidToken) return false;
-      return 'valid token'
+
+      return { user: updatedUser, result: 'valid token' };
     } catch (error) {
-      throw new BadRequestException('Invalid token')
+      throw new BadRequestException('Invalid token');
     }
   }
 }
