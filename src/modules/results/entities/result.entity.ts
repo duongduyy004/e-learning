783a1891b306cd9572396c58a387e601051240c5
duongduyy004 <CT070213@actvn.edu.vn>
2025-12-04 15:50:28 +0700

    fix(result): fix auto set isCompleted

--- src/modules/results/entities/result.entity.ts
+++ src/modules/results/entities/result.entity.ts
@@ -45,7 +45,7 @@ export class ResultEntity {
 
   @BeforeUpdate()
   updateResultStatus() {
-    if (this.currentIndex === this.questionIds.length - 1)
+    if (this.currentIndex > this.questionIds.length - 1)
       this.isComplete = true;
   }
 }
